425e4dc4829469f02c8fb9a33da8370797b94952
Aluizio Cidral Júnior <cidraljunior@hotmail.com>
2020-10-02 09:54:15 -0300

    Aulas 2 e 3

--- aula_02.sql
+++ aula_02.sql
@@ -1,2 +1,10 @@
+/*
+Desafio 1
+
+A equipe de marketing precisa fazer uma pesquisa sobre os nomes mais comums de seus clientes.
+E precisa do nome e sobrenome de todos os clientes que estão cadastrados no sistema.
+
+*/
+
 SELECT FirstName, LastName 
 FROM person.Person
