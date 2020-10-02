a3565166d1e737526097324dc3a18cbc3611d8cd
Aluizio Cidral Júnior <cidraljunior@hotmail.com>
2020-10-02 09:47:00 -0300

    Aulas 2 e 3

---
+++ aula_02.sql
@@ -0,0 +1,2 @@
+SELECT FirstName, LastName 
+FROM person.Person
---
+++ aula_03.sql
@@ -0,0 +1,11 @@
+/*
+Desafio 2
+
+Quantos sobrenomes únicos temos em nossa tabela person.Person
+
+*/
+
+SELECT DISTINCT lastName
+FROM person.Person;
+
+-- 1206 sobrenomes distintos
