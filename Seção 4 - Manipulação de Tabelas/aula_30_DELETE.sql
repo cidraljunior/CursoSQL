3c1607d7f8f498b84d28f3c79f6a989544d862de
Aluizio Cidral Júnior <cidraljunior@hotmail.com>
2020-10-13 10:50:46 -0300

    aula 30

---
+++ Seção 4 - Manipulação de Tabelas/aula_30_DELETE.sql
@@ -0,0 +1,19 @@
+/*
+DELETE
+
+Apagar linhas do banco de dados
+
+Importante: filtrar as linhas para que não apague toda a tabela
+
+Sintaxe
+
+DELETE FROM nomeTabela
+WHERE condicao
+
+*/
+
+SELECT *
+FROM aula
+
+DELETE FROM aula
+WHERE nome = 'mudei'
