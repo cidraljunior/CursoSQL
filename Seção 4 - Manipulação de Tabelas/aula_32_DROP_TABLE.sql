67aaf73941955e21611b42c9d3a01b66c80908cb
Aluizio Cidral Júnior <cidraljunior@hotmail.com>
2020-10-13 11:10:03 -0300

    aula 32

---
+++ Seção 4 - Manipulação de Tabelas/aula_32_DROP_TABLE.sql
@@ -0,0 +1,24 @@
+/*
+DROP TABLE
+
+Exclui a tabela do banco de dados
+
+Sintaxe
+
+DROP TABLE nomeDaTabela
+
+*/
+
+SELECT * FROM YouTube2
+
+DROP TABLE YouTube2
+
+/*
+TRUNCATE
+
+Apaga os dados da tabela, mas a tabela não.
+
+Sintaxe
+
+TRUNCATE TABLE nomeDaTabela
+*/
